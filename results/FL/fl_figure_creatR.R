5303d21c8f3fe93423e9c05c6544448fdca02108
jcuriel-unc <47302709+jcuriel-unc@users.noreply.github.com>
2020-11-03 21:37:07 -0500

    FL updates

---
+++ results/FL/fl_figure_creatR.R
@@ -0,0 +1,140 @@
+###################################################################################
+####################### Election Results CleanR #################################
+################################################################################
+library(ggplot)
+library(dplyr)
+library(stringi)
+library(stringr)
+options(stringsAsFactors = FALSE)
+################################################################################
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+caption_date <- paste0("Graph Source: MIT Elections Data and Science Lab\nGraph date:",
+                       sep=" ", format(Sys.Date(),format="%m/%d/%Y"))
+list.files()
+medsl_brands <- c("#3791FF","#59CBF5","#C0BA79","#F6573E","#156DD0","#C72654","#FF6878")
+
+####This script will be used to create ggplot style figures for election night for counties. 
+
+
+#Step 1: Read in data 
+county_metro <- read.csv("F:/MEDSL/election_night2020/context/acs_demos/complete_county_data.csv")
+county_metro$Geo_FIPS <- str_pad(county_metro$Geo_FIPS, width=5,side="left",pad="0")
+
+
+county_results <- read.csv()  #specify election results here
+
+county_results <- readRDS("F:/MEDSL/election_night2020/context/historical_elections/county_prez_wide.rds")
+county_results$dem_pct <- (county_results$democrat/(county_results$democrat+county_results$republican+county_results$other))*100
+county_results <- as.data.frame(county_results)
+county_results$total_vote <- county_results$democrat + county_results$republican + county_results$other
+county_results <- subset(county_results, year==2016)
+
+##Step 2: Specify county FIPs field 
+
+county_field <- readline(prompt="Enter name of column field with the county FIPs code, length of 5 : ")
+county_results[,which(colnames(county_results)==county_field)] <- 
+  str_pad(county_results[,which(colnames(county_results)==county_field)], width=5,side="left",pad="0")
+##merging data 
+county_metro2 <- merge(county_metro, county_results, by.x="Geo_FIPS", by.y=county_field, all.x=T)
+county_metro2$metro_factor <- factor(county_metro2$metro_type, levels=c("Large Metro","Medium Metro","Small Metro",
+                                                                                "Micro","Noncore"))
+
+### step 3: Select dem_pct variable 
+party_field <- readline(prompt="Enter name of column field with the Democratic 2 party vote share data, as percent : ")
+col_nump <- which(colnames(county_metro2)==party_field)
+colnames(county_metro2)[col_nump] <- "dem_pct"
+
+##step 4: Select democrat and total vote fields 
+dem_count_field <- readline(prompt="Enter name of column field with the Democratic 2 party vote share data, as count : ")
+total_vote_field <- readline(prompt="Enter name of column field with the total 2 party vote share data, as count : ")
+dem_tally_num <- which(colnames(county_metro2)==dem_count_field)
+total_tally_num <- which(colnames(county_metro2)==total_vote_field)
+###adding fields just in case 
+county_metro2$dem_total <- county_metro2[,dem_tally_num]
+county_metro2$total_vote_field <- county_metro2[,total_tally_num]
+
+#### Now, the following are plots that can be created by demographics and the like. 
+county_metro_sums <- county_metro2 %>% group_by(metro_type) %>% 
+  summarise(dem_vote=sum(dem_total,na.rm=T),
+            total_vote=sum(total_vote_field,na.rm=T))
+county_metro_sums$dem_pct <- (county_metro_sums$dem_vote/county_metro_sums$total_vote)*100
+###setting factors 
+county_metro_sums$metro_factor <- factor(county_metro_sums$metro_type, levels=c("Large Metro","Medium Metro","Small Metro",
+                                                                                "Micro","Noncore"))
+
+natl_metro_plot <- ggplot(county_metro_sums, aes(y=dem_pct,x=metro_factor,fill=metro_factor)) + 
+  geom_bar(position="stack", stat="identity") + theme_minimal() + 
+  scale_fill_manual(values = medsl_brands[c(1:4,6)],drop=F)  +  scale_y_continuous(labels = function(x) paste0(x, "%"), limits = c(0,100))  +
+  labs(title="Democratic Vote Share by Metro Designation",x="Metro Type",y="Democratic vote %", fill="Metro Type",
+       caption = paste0(caption_date , "\nMetro areas categorized via National Center for Health Statistics coding.")) +
+  theme(plot.caption = element_text(hjust=0)) 
+natl_metro_plot
+
+###Select file title name 
+file_plot_name <- readline(prompt="Enter file name of plot : ")
+ggsave(paste0(file_plot_name,sep="", ".png"), plot = natl_metro_plot, scale = 1,
+       width = 9, height = 6, units = c("in"), dpi = 600) 
+
+
+
+####################### Now scatter plot for demographics ################################################
+####Note: Subset as appropriate here: 
+decision_to_sub <- readline(prompt="Subset data? Y/N: ")
+if(decision_to_sub=="Y"){
+  state_choice <- readline(prompt="Select state via postal code : ")
+  state_choice <- str_to_upper(state_choice)
+  county_metro2 <- subset(county_metro2, state_po==state_choice)
+  rows_num <- as.character(nrow(county_metro2))
+  msg_state <- paste0("Chose to subset data to", sep=" ",unique(county_metro2$state),sep=". ", "There are a total of ", sep=" ",
+                      rows_num, "observations within the subsetted data.")
+  print(msg_state)
+}else{
+  row_nums2 <- as.character(nrow(county_metro2))
+  msg_state2 <- paste0("Chose not to subset. Will therefore plot", sep=" ", row_nums2,sep=" ", "counties as observations.")
+  print(msg_state2)
+}
+
+
+names(county_metro2)
+variable_selection_x <- readline(prompt="Enter variable name to plot as x-axis : ")
+variable_selection_y <- readline(prompt="Enter variable name to plot as y-axis : ")
+size_selection <- readline(prompt="Enter variable name to size plot (i.e. pop10 for county total pop.) : ")
+
+###titles 
+x_title <- readline(prompt="Enter title for plot x-axis : ")
+y_title <- readline(prompt="Enter title for plot y-axis : ")
+main_title <- readline(prompt="Enter title for plot : ")
+file_demo_plot <- readline(prompt="Enter file name for plot : ") 
+###assigning vars to new columns to ease reading 
+county_metro2$var_x <- county_metro2[,which(colnames(county_metro2)==variable_selection_x)]
+county_metro2$var_y <- county_metro2[,which(colnames(county_metro2)==variable_selection_y)]
+county_metro2$var_size <- county_metro2[,which(colnames(county_metro2)==size_selection)]
+
+if(max(county_metro2$var_x,na.rm=T) > 100 | min(county_metro2$var_x,na.rm=T) < 0 ){
+  print("Warning: The x variable is outside the 0 - 100 range. Please modify, or be sure to manually modify the ggplot code.")
+}else{
+  print("X  values appear fine.")
+}
+
+if(max(county_metro2$var_y,na.rm=T) > 100 | min(county_metro2$var_y,na.rm=T) < 0 ){
+  print("Warning: The y variable is outside the 0 - 100 range. Please modify, or be sure to manually modify the ggplot code.")
+}else{
+  print("y  values appear fine.")
+}
+
+
+###plot here; set to group by metro type 
+demo_plot <- ggplot(county_metro2,aes(y=var_y,x=var_x,size=var_size,color=metro_factor)) +
+  geom_point(alpha=0.4) + scale_color_manual(values = medsl_brands[c(1:4,6)],drop=F) + theme_minimal() +
+  guides(size=FALSE)  + labs(title=main_title,x=x_title,y=y_title, color="Metro Type",
+                            caption = paste0(caption_date , "\nMetro areas categorized via National Center for Health Statistics coding."))+
+  scale_y_continuous(labels = function(x) paste0(x, "%"), limits = c(0,100)) + 
+  scale_x_continuous(labels = function(x) paste0(x, "%"), limits = c(0,100)) + theme(plot.caption = element_text(hjust=0)) 
+
+demo_plot
+ggsave(paste0(file_demo_plot,sep="", ".png"), plot = demo_plot, scale = 1,
+       width = 9, height = 6, units = c("in"), dpi = 600) 
+
+
+
+
